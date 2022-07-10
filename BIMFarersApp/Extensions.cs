6b849f48061f106994d782414cf1e8686c96d6dd
Steven Mah <chunho01@hotmail.com>
2022-07-10 20:44:28 +1000

    Add project files.

---
+++ BIMFarersApp/Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace BIMFarersApp
+{
+    public static class Extensions
+    {
+        public static DateTime RoundDownToNearest30(this DateTime datetime)
+        {
+            double minutes = datetime.TimeOfDay.TotalMinutes % 30;
+            return datetime.AddMinutes(-minutes);
+        }
+
+        public static DateTime RoundUpToNearest30(this DateTime datetime)
+        {
+            double atMinuteInBlock = datetime.TimeOfDay.TotalMinutes % 30;
+            double minutesToAdd = 30 - atMinuteInBlock;
+            return datetime.AddMinutes(minutesToAdd);
+        }
+    }
+}
